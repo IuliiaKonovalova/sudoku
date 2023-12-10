88bfb1541fbdbe2f787af6b70906d6757448d267
IuliiaKonovalova <JuliiaKonn@protonmail.com>
2023-12-09 19:49:29 -0600

    feat: add solve logic

--- Main.java
+++ Main.java
@@ -77,5 +77,25 @@ public class SudokuSolver {
         allowedInBox(board, number, row, column);
   }
 
-
+  private static boolean solveBoard(int[][] board) {
+    for (int row = 0; row < GRID_SIZE; row++) {
+      for (int column = 0; column < GRID_SIZE; column++) {
+        if (board[row][column] == 0) {
+          for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+            if (isAllowed(board, numberToTry, row, column)) {
+              board[row][column] = numberToTry;
+              if (solveBoard(board)) {
+                return true;
+              }
+              else{
+                board[row][column] = 0;
+              }
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
